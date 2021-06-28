26449c4fc288df206ec1c27bac891c8811323ef1
Andreas Fett <a.fett@gmx.de>
2021-06-28 09:48:26 +0200

    add some comments

--- overload-set.h
+++ overload-set.h
@@ -6,31 +6,33 @@
 template <typename ...H> class OverloadSet;
 
 template <typename T>
-class OverloadSet<T> : public T
+class OverloadSet<T> : public T // inherit from the lamda type and terminate recursive inheritance
 {
 public:
 	OverloadSet(T && t)
 	:
-		T(std::forward<T>(t))
+		T(std::forward<T>(t))  // call the ctor of the lamda type
 	{ }
 
-	using T::operator();
+	using T::operator();  // pull the lambda call operator into overload resolution for this class
 };
 
 template <typename T, typename ...Types>
-class OverloadSet<T, Types...> : public T, public OverloadSet<Types...>
+class OverloadSet<T, Types...> : public T, public OverloadSet<Types...>    // inherit from one lamda type and recurse over the lamda type list
 {
 public:
 	OverloadSet(T && t, Types && ...ts)
 	:
-		T(std::forward<T>(t)),
-		OverloadSet<Types...>(std::forward<Types>(ts)...)
+		T(std::forward<T>(t)),                              // call the ctor of the lamda type
+		OverloadSet<Types...>(std::forward<Types>(ts)...)   // call the ctor of the base class
 	{}
 
-	using T::operator();
-	using OverloadSet<Types...>::operator();
+	using T::operator();                         // pull the lambda call operator into overload resolution for this class
+	using OverloadSet<Types...>::operator();     // pull the call operator(s) of the base class(es) into overload resolution for this class
 };
 
+
+// deduce the lambda types with this helper function
 template <typename ...F>
 auto overload(F && ...f) -> decltype(OverloadSet<F...>(std::forward<F>(f)...))
 {
